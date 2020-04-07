469932ebee91bf2696ada031114d8afe91ba4cfc
RKRitik <khatri.ritik16@gmail.com>
2020-04-07 16:35:57 +0530

    Updates

---
+++ precache-manifest.5088a2fdc168f7fca5847a15ffe73f77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd0e7f745b51cc53637f1cb22ab47a2",
+    "url": "/Simple-Shopping-Website/index.html"
+  },
+  {
+    "revision": "fc4812433c303da32739",
+    "url": "/Simple-Shopping-Website/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2553d459b61bcd31a0e2",
+    "url": "/Simple-Shopping-Website/static/css/main.3dffcfd2.chunk.css"
+  },
+  {
+    "revision": "fc4812433c303da32739",
+    "url": "/Simple-Shopping-Website/static/js/2.05569c8e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Simple-Shopping-Website/static/js/2.05569c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2553d459b61bcd31a0e2",
+    "url": "/Simple-Shopping-Website/static/js/main.d950783a.chunk.js"
+  },
+  {
+    "revision": "9a6c2113d2db9c82fdb2",
+    "url": "/Simple-Shopping-Website/static/js/runtime-main.1159bad3.js"
+  }
+]);
